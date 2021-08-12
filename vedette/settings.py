d34319bbaa8162d39110895e72322080036c2d7d
Matigakis Panagiotis <pmatigakis@gmail.com>
2021-08-12 22:37:01 +0300

    Added support for loading .env files

--- vedette/settings.py
+++ vedette/settings.py
@@ -10,8 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from distutils.util import strtobool
 from os import getenv, environ
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=getenv("ENV_PATH"))
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eb1*zd$+_y_@l37^691(*ww$_4vy4195ie-@hpmuct!u^^3gt1'
+SECRET_KEY = environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(strtobool(getenv("DEBUG", "false")))
 
 ALLOWED_HOSTS = []
 
