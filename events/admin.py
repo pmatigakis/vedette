1cf583c3216b38c43a4e9f9eb7076db7a1e3de34
Matigakis Panagiotis <pmatigakis@gmail.com>
2021-08-23 20:25:52 +0300

    Added the event model

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Project
+from .models import Project, Event
 
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -10,3 +10,32 @@ class ProjectAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Project, ProjectAdmin)
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "view_project_title",
+        "timestamp"
+    )
+
+    fields = (
+        "id",
+        "view_project_title",
+        "timestamp",
+        "created_at",
+        "updated_at"
+    )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    @admin.display(ordering='project__title', description="Project")
+    def view_project_title(self, obj):
+        return obj.project.title
+
+
+admin.site.register(Event, EventAdmin)
--- events/models.py
+++ events/models.py
@@ -4,6 +4,11 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 
+SUPPORTED_PLATFORMS = (
+    ("python", "Python"),
+)
+
+
 class Project(models.Model):
     title = models.CharField(
         max_length=120,
@@ -31,3 +36,53 @@ class Project(models.Model):
         blank=False,
         null=False
     )
+
+
+class Event(models.Model):
+    id = models.UUIDField(
+        blank=False,
+        null=False,
+        primary_key=True
+    )
+
+    project = models.ForeignKey(
+        "Project",
+        blank=False,
+        null=False,
+        on_delete=models.CASCADE
+    )
+
+    timestamp = models.DateTimeField(null=False, blank=False)
+
+    platform = models.CharField(
+        max_length=32,
+        choices=tuple(SUPPORTED_PLATFORMS),
+        null=False,
+        blank=False
+    )
+
+    data = models.JSONField(blank=False, null=False)
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        blank=False,
+        null=False
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        blank=False,
+        null=False
+    )
+
+    class Meta:
+        indexes = [
+            models.Index(
+                fields=["project_id"],
+                name="ix__event__project_id__project"
+            ),
+            models.Index(
+                fields=["timestamp"],
+                name="ix__event__timestamp"
+            )
+        ]
