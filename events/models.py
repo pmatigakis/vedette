1a45d6d9aeadc79a20b12bcdd32f38c9555a8cc1
Matigakis Panagiotis <pmatigakis@gmail.com>
2021-09-12 15:06:30 +0300

    The ui will show some addition event information

--- events/models.py
+++ events/models.py
@@ -62,22 +62,99 @@ class Event(models.Model):
 
     def message(self):
         if (
-                "logentry" in self.data and
-                self.data["logentry"] and
-                self.data["logentry"]["message"]
+            "exception" in self.data and
+            self.data["exception"] and
+            self.data["exception"]["values"]
+        ):
+            return self.data["exception"]["values"][0]["type"]
+        elif (
+            "logentry" in self.data and
+            self.data["logentry"] and
+            self.data["logentry"]["message"]
         ):
             return self.data["logentry"]["message"] % tuple(
-                self.data["logentry"]["params"])
-        elif (
-                "exception" in self.data and
-                self.data["exception"] and
-                self.data["exception"]["values"]
-        ):
-            return (
-                f'{self.data["exception"]["values"][0]["type"]} - '
-                f'{self.data["exception"]["values"][0]["value"]}'
-            )
+                self.data["logentry"]["params"] or [])
         elif "message" in self.data and self.data["message"]:
             return self.data["message"]
 
         return f"Event - {self.id}"
+
+    def logger(self):
+        return self.data.get("logger")
+
+    def level(self):
+        return self.data.get("level")
+
+    def transaction(self):
+        return self.data.get("transaction")
+
+    def exception_message(self):
+        if (
+            "exception" in self.data and
+            self.data["exception"] and
+            self.data["exception"]["values"]
+        ):
+            return self.data["exception"]["values"][0]["value"]
+
+    def user_tag_value(self):
+        user_data = self.data.get("user", {})
+        return (
+                user_data.get("id") or
+                user_data.get("username") or
+                user_data.get("email") or
+                user_data.get("ip_address")
+        )
+
+    def runtime_tag_value(self):
+        runtime = self.data.get("contexts", {}).get("runtime", {})
+
+        components = []
+        if "name" in runtime:
+            components.append(runtime["name"])
+        if "version" in runtime:
+            components.append(runtime["version"])
+
+        runtime_tag = "-".join(components)
+        if not  runtime_tag:
+            runtime_tag = None
+
+        return runtime_tag
+
+    def user_defined_tags(self):
+        return self.data.get("tags", {})
+
+    def environment(self):
+        return self.data.get("environment")
+
+    def handled(self):
+        exceptions = self.data.get("exception", {}).get("values")
+        if exceptions and exceptions[0]["mechanism"]:
+            return exceptions[0]["mechanism"].get("handled") or False
+
+        return False
+
+    def mechanism(self):
+        if "exception" in self.data:
+            exception_values = self.data["exception"].get("values", [])
+            if exception_values:
+                return exception_values[0].get("mechanism", {}).get("type")
+        elif "logentry" in self.data:
+            return "logging"
+
+        return None
+
+    def runtime_name(self):
+        return self.data.get("contexts", {}).get("runtime", {}).get("name")
+
+    def server_name(self):
+        return self.data.get("server_name")
+
+    def log_message(self):
+        message = self.data.get("logentry", {}).get("message")
+        if message:
+            return message % tuple(self.data.get("logentry", {}).get("params", []))
+
+        return None
+
+    def log_params(self):
+        return self.data.get("logentry", {}).get("params", [])
