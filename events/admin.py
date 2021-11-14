e0b608c923333657a86ef02d376b4cda1ae31982
Matigakis Panagiotis <pmatigakis@gmail.com>
2021-11-14 23:08:11 +0200

    Added the admin panel for the issues

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Event, RawEvent
+from .models import Event, RawEvent, Issue
 
 
 class RawEventAdmin(admin.ModelAdmin):
@@ -25,6 +25,7 @@ class EventAdmin(admin.ModelAdmin):
     fields = (
         "id",
         "raw_event",
+        "issue",
         "view_project_name",
         "message",
         "timestamp",
@@ -44,5 +45,40 @@ class EventAdmin(admin.ModelAdmin):
         return obj.project.name
 
 
+class IssueAdmin(admin.ModelAdmin):
+    list_display = (
+        "id", "view_project_name", "first_seen_at", "last_seen_at", "resolved"
+    )
+
+    fields = (
+        "id",
+        "signature",
+        "primary_event",
+        "view_project_name",
+        "view_message",
+        "resolved",
+        "created_at",
+        "updated_at",
+        "resolved_at",
+        "first_seen_at",
+        "last_seen_at"
+    )
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    @admin.display(ordering="project__name", description="Project")
+    def view_project_name(self, obj):
+        return obj.project.name
+
+    @admin.display(ordering="primary_event__message", description="Message")
+    def view_message(self, obj):
+        return obj.primary_event.message
+
+
 admin.site.register(Event, EventAdmin)
 admin.site.register(RawEvent, RawEventAdmin)
+admin.site.register(Issue, IssueAdmin)
